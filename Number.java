2f93beb41eb3d1bfd5500c38df313fb15f5b85e4
jeevajeeva <vjeeva522@gmail.com>
2017-07-05 12:02:45 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,20 @@
+# Number
+import java.io.*;
+import java.util.*;
+public class Number
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n,reverse=0;
+    int a=sc.nextInt();
+    n=a;
+    while(a>0)
+    {
+      int b=a%10;
+      reverse=reverse*10+b;
+      a=a/10;
+    }
+    System.out.println(reverse);
+  }
+}
